51143ab17389e7bf974c5edfa490906434cb1a50
ariana124 <ariana.bibiano@gmail.com>
2020-08-11 13:32:42 -0700

    console logs a JSON object containing the information from the weather api

--- app.js
+++ app.js
@@ -10,8 +10,13 @@ app.get("/", function(req, res) {
 
     const url = "https://api.openweathermap.org/data/2.5/weather?zip=94603&appid=f6fad539d556a5c0b26ab209c1c2c3c0&units=imperial";
     https.get(url, function(response) {
-        console.log(response);
+        console.log(response.statusCode);
+
+        response.on("data", function(data) {
+            const weatherData = JSON.parse(data);
+            console.log(weatherData);
+        })
     });
 
-    //res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + "/index.html");
 });
