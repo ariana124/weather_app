00f9be26c72d17ec60bb9483116f2eb374b2959b
ariana124 <ariana.bibiano@gmail.com>
2020-08-11 13:15:43 -0700

    used https module to send get request to the open weather api

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const https = require("https");
 const app = express();
 
 app.listen(3000, function() {
@@ -6,5 +7,11 @@ app.listen(3000, function() {
 });
 
 app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/index.html");
+
+    const url = "https://api.openweathermap.org/data/2.5/weather?zip=94603&appid=f6fad539d556a5c0b26ab209c1c2c3c0&units=imperial";
+    https.get(url, function(response) {
+        console.log(response);
+    });
+
+    //res.sendFile(__dirname + "/index.html");
 });
