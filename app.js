2e0b7846347e0476749c8f74d1dab129a6aa6e8b
ariana124 <ariana.bibiano@gmail.com>
2020-08-12 13:08:39 -0700

    updated url with different zip code

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.listen(3000, function() {
 
 app.get("/", function(req, res) {
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?zip=94603&appid=f6fad539d556a5c0b26ab209c1c2c3c0&units=imperial";
+    const url = "https://api.openweathermap.org/data/2.5/weather?zip=94612&appid=f6fad539d556a5c0b26ab209c1c2c3c0&units=imperial";
     https.get(url, function(response) {
         console.log(response.statusCode);
 
